dc8a756cdeebcd6291fe2d7f852b67aa39ae379b
Leonardo Ferreira <dossantosferreiral@student.douglascollege.ca>
2021-03-10 17:10:54 -0800

    added addition functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import React, { useState } from 'react'
 import CalculatorForm from './Components/CalculatorForm/CalculatorForm'
 
 function App() {
+  const [operation, setOpetarion] = useState({});
+
+  function updateOperands(e) {
+    setOpetarion({
+      ...operation,
+      [e.target.id] : e.target.value
+    })
+  }
+
+  function doOperation() {
+    console.log(operation.operation)
+    switch(operation.operation) {
+      case "add": 
+      console.log("Results: " + parseFloat(operation.operand1) + parseFloat(operation.operand2))
+      break;
+      default: 
+      console.log("Sorry, We don't do that")
+    }
+  }
+
   return (
     <div className="container">
       <h1>Calculator App</h1>
-      <CalculatorForm />
+      <CalculatorForm update={updateOperands} action={doOperation}/>
     </div>
   );
 }
--- src/Components/CalculatorForm/CalculatorForm.js
+++ src/Components/CalculatorForm/CalculatorForm.js
@@ -6,20 +6,21 @@ function CalculatorForm(props) {
             <div className="row">
                 <div className="col-4">
                     <label for="operand1">Operand 1</label>
-                    <input type="text" className="form-control-plaintext" placeholder="Operand 1" onChange={props.update}></input>
+                    <input type="text" className="form-control-plaintext" placeholder="Operand 1" onChange={props.update} id="operand1"></input>
                 </div>
             </div>
             <div className="row">
                 <div className="col-4">
-                    <select className="form-select">
+                    <select className="form-select" id="operation" onChange={props.operation}>
                         <option selected>Please Select an Operation</option>
+                        <option selected>Addition</option>
                     </select>
                 </div>
             </div>
             <div className="row">
                 <div className="col-4">
                     <label for="operand2">Operand 2</label>
-                    <input type="text" className="form-control-plaintext" placeholder="Operand 2" onChange={props.update}></input>
+                    <input type="text" className="form-control-plaintext" placeholder="Operand 2" onChange={props.update} id="operand2"></input>
                 </div>
             </div>
             <button onClick={props.action}>Calculate</button> 
